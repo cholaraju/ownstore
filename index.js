dd46dd7b1b82b8057de0fe100bde0585475b45e3
rajuneluri <rajuneluri@gmail.com>
2024-09-01 17:27:07 +0530

    changed the cards using async and await

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-fetch("https://fakestoreapi.com/products")
-  .then((data) => {
-    return data.json();
-  })
-  .then((completeddata) => {
-    // console.log(completeddata[2].title);
+async function fetchProducts() {
+  try {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const products = await response.json();
 
-    let data1 = "";
-    completeddata.map((values) => {
-      data1 += ` <div class="card">
-            <h1 class="title">${values.title}</h1>
-            <img src=${values.image}
-                alt="" class="images">
-            <p>${values.description}</p>
-            <p class="category">${values.category}</p>
-            <p class="price">${values.price}</p>
+    const cardsContainer = document.getElementById("cards");
+    cardsContainer.innerHTML = products
+      .map(
+        (product) => `
+      <div class="card">
+        <h1 class="title">${product.title}</h1>
+        <img src="${product.image}" alt="" class="images">
+        <p>${product.description}</p>
+        <p class="category">${product.category}</p>
+        <p class="price">${product.price}</p>
+      </div>
+    `
+      )
+      .join("");
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  }
+}
 
-        </div>`;
-    });
-    document.getElementById("cards").innerHTML = data1;
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+fetchProducts();
